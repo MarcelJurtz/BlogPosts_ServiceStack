1e8599a2e886a52d2f2210b762f869c0316052f1
MarcelJurtz <jurtzmarcel@gmail.com>
2017-11-25 18:43:17 +0100

    Add project files for blogpost two

---
+++ Part_2_Basic_Service/BlogPosts_ServiceStack/ExpensesService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceStack
+{
+    public class ExpensesService : Service
+    {
+        public object Post(Expense request)
+        {
+            return new ExpenseResponse
+            {
+                Amount = request.Amount,
+                Total = 500,
+                Status = "OK"
+            };
+        }
+    }
+
+    [Route("/Expense")]
+    [Route("/Expense/{Amount}")]
+    public class Expense
+    {
+        public double Amount { get; set; }
+    }
+    
+    public class ExpenseResponse
+    {
+        public double Amount { get; set; }
+        public double Total { get; set; }
+        public String Status { get; set; }
+    }
+}
